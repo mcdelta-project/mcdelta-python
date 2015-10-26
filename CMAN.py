bb8da02e9f34a14aebe0162e828cb8dc5e8c6f73
williambl <william@williambl.com>
2015-10-26 10:49:40 +0000

    Add Functions
    Functions. Functions everywhere.

--- CMAN.py
+++ CMAN.py
@@ -12,20 +12,39 @@ os.chdir("Data")
 for file in glob.glob("*"):
     os.remove(file)
 
-modname = input("Enter the mod that you wish to download.")
+command = input()
 
-print("Downloading archive...")
-url = "http://modlist.mcf.li/api/v3/1.8.json"  # JSON link
-file_name = "CMAN.json"
+if(command == "update"):
+	update_archive()
+if(command == "install"):
+	update_archive()
+	find_mod()
+	install_mod()
+if(command == "remove"):
+	remove_mod()
 
-# Download it.
-with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
-    shutil.copyfileobj(response, out_file)
-print("Done.")
 
-with open("CMAN.json") as json_file:  # JSON parsing
+def update_archive:
+	print("Downloading archive...")
+	url = "http://modlist.mcf.li/api/v3/1.8.json"  # JSON link
+	file_name = "CMAN.json"
+	# Download it.
+	with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+	    shutil.copyfileobj(response, out_file)
+	print("Done.")
+
+
+def find_mod:
+	mod = input("Enter mod name.")
+	with open("CMAN.json") as json_file:  # JSON parsing
     json_data = json.load(json_file)
     pass  # I need help getting the download link from the JSON...
 
 
-pass  # Add code to download file and move to mods directory
+def install_mod:
+	pass  # Add code to download file and move to mods directory
+
+
+def remove_mod:
+	mod = input("Enter mod name.")
+	pass
