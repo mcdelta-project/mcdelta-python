96a379fadd93a21949b9b5e461925895b5ab66f4
williambl <william@williambl.com>
2017-10-18 21:00:10 +0100

    Made tests work with a clean installation

--- run_tests.py
+++ run_tests.py
@@ -4,6 +4,12 @@ import sys
 print("Running tests on " + sys.platform)
 deltamc = pexpect.spawn("python3 ./deltamc.py", encoding="UTF-8")
 deltamc.logfile = sys.stdout
+deltamc.expect("Enter mod folder location for instance default \(absolute path\)\: ")
+deltamc.sendline(".")
+deltamc.expect("Enter jar folder location for instance default \(absolute path\)\:")
+deltamc.sendline(".")
+deltamc.expect("Enter Minecraft version for instance default:")
+deltamc.sendline("1.12")
 deltamc.expect("> ")
 deltamc.sendline("install MinecraftForge")
 deltamc.expect("Done. Please run the installer.")
